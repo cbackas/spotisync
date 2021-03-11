2e71a5ea9f8f142b32569bc00ab58aba6a93d4c4
Zac Gibson <zgibson@live.com>
2021-03-11 14:50:32 -0600

    only print all these errors if verbose mode is on

--- spotisync/utils.py
+++ spotisync/utils.py
@@ -1,10 +1,13 @@
+from os import getenv
 from datetime import datetime
 
 def current_timestamp():
     return datetime.now().strftime("%Y/%m/%d %H:%M:%S")
 
 def log_error(error_text):
-    log(f'[ERROR] {error_text}')
+    verbose = getenv('verbose', 'false')
+    if (verbose.lower == 'true'):
+        log(f'[ERROR] {error_text}')
 
 def log(text):
     print(f'[{current_timestamp()}] {text}')
