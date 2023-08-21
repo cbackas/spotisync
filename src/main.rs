f8cb771becfe1c1951395ac0a162838e70e133d0
cbackas <zgibson@live.com>
2023-08-21 00:02:40 -0500

    fix runtime issue

--- src/main.rs
+++ src/main.rs
@@ -5,33 +5,27 @@ use tokio::{join, runtime::Builder};
 
 mod spotify;
 
-#[tokio::main]
-async fn main() {
-    let spotify = spotify::auth::get_spotify_client().await;
-
-    let source_playlist_id: String = match env::var_os("SYNC_SOURCE_PLAYLIST_ID") {
-        Some(v) => v.into_string().unwrap(),
-        None => panic!("Missing SYNC_SOURCE_PLAYLIST_ID env var"),
-    };
-    let source_playlist_id: PlaylistId = PlaylistId::from_id_or_uri(&source_playlist_id)
-        .expect("Failed to parse source playlist id");
-
-    let target_playlist_id: String = match env::var_os("SYNC_TARGET_PLAYLIST_ID") {
-        Some(v) => v.into_string().unwrap(),
-        None => panic!("Missing SYNC_TARGET_PLAYLIST_ID env var"),
-    };
-    let target_playlist_id: PlaylistId = PlaylistId::from_id_or_uri(&target_playlist_id)
-        .expect("Failed to parse target playlist id");
-
-    let continous: bool = match env::var_os("CONTINUOUS_SYNC") {
-        Some(v) => v.into_string().unwrap() == "true",
-        None => false,
-    };
-
+fn main() {
     let rt = Builder::new_current_thread().enable_all().build().unwrap();
 
     rt.block_on(async {
         let playlist_sync_task = async {
+            let spotify = spotify::auth::get_spotify_client().await;
+
+            let source_playlist_id: String = match env::var_os("SYNC_SOURCE_PLAYLIST_ID") {
+                Some(v) => v.into_string().unwrap(),
+                None => panic!("Missing SYNC_SOURCE_PLAYLIST_ID env var"),
+            };
+            let source_playlist_id: PlaylistId = PlaylistId::from_id_or_uri(&source_playlist_id)
+                .expect("Failed to parse source playlist id");
+
+            let target_playlist_id: String = match env::var_os("SYNC_TARGET_PLAYLIST_ID") {
+                Some(v) => v.into_string().unwrap(),
+                None => panic!("Missing SYNC_TARGET_PLAYLIST_ID env var"),
+            };
+            let target_playlist_id: PlaylistId = PlaylistId::from_id_or_uri(&target_playlist_id)
+                .expect("Failed to parse target playlist id");
+
             loop {
                 spotify::playlist_sync::one_way_sync(
                     spotify.clone(),
@@ -39,10 +33,6 @@ async fn main() {
                     &target_playlist_id,
                 )
                 .await;
-
-                if !continous {
-                    break;
-                }
 
                 // sleep 40 seconds
                 std::thread::sleep(std::time::Duration::from_secs(40));
