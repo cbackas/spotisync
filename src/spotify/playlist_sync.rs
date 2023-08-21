5e1311eac27f6b8857e400f63eb913734f6ee38b
cbackas <zgibson@live.com>
2023-08-20 23:17:19 -0500

    add a rate limit delay thing

--- src/spotify/playlist_sync.rs
+++ src/spotify/playlist_sync.rs
@@ -1,5 +1,6 @@
 use futures_util::TryStreamExt;
 use rspotify::{
+    http::HttpError,
     model::{FullTrack, PlayableItem, PlaylistId, PlaylistItem},
     prelude::*,
     AuthCodeSpotify,
@@ -17,16 +18,14 @@ pub async fn one_way_sync(
     }
 
     // fetch all tracks in both playlists
-    let source_playlist: Vec<PlaylistItem> = spotify
-        .playlist_items(source_playlist_id.as_ref(), None, None)
-        .try_collect()
-        .await
-        .expect("Error fetching source playlist");
-    let target_playlist: Vec<PlaylistItem> = spotify
-        .playlist_items(target_playlist_id.as_ref(), None, None)
-        .try_collect()
-        .await
-        .expect("Error fetching target playlist");
+    let source_playlist: Vec<PlaylistItem> =
+        get_playlist_tracks(spotify.clone(), source_playlist_id)
+            .await
+            .unwrap_or(Vec::new());
+    let target_playlist: Vec<PlaylistItem> =
+        get_playlist_tracks(spotify.clone(), target_playlist_id)
+            .await
+            .unwrap_or(Vec::new());
 
     // determine the tracks that are in the source playlist but not in the target playlist
     let unsynced_tracks: Vec<PlayableId> = get_unsynced_tracks(source_playlist, target_playlist)
@@ -50,6 +49,48 @@ pub async fn one_way_sync(
     }
 }
 
+async fn get_playlist_tracks(
+    spotify: AuthCodeSpotify,
+    playlist_id: &PlaylistId<'_>,
+) -> anyhow::Result<Vec<PlaylistItem>> {
+    loop {
+        let spotify_response = spotify
+            .playlist_items(playlist_id.as_ref(), None, None)
+            .try_collect()
+            .await;
+
+        match spotify_response {
+            Ok(playlist_items) => return Ok(playlist_items),
+            Err(rspotify::ClientError::Http(http_error)) => match *http_error {
+                HttpError::StatusCode(response) => {
+                    if response.status().as_u16() == 429 {
+                        println!("Rate limited, waiting 20 seconds");
+                        std::thread::sleep(std::time::Duration::from_secs(20));
+                        continue;
+                    } else {
+                        return Err(anyhow::Error::msg(format!(
+                            "Error getting playlist tracks: {}",
+                            response.status()
+                        )));
+                    }
+                }
+                _ => {
+                    return Err(anyhow::Error::msg(format!(
+                        "Error getting playlist tracks: {}",
+                        http_error.to_string()
+                    )))
+                }
+            },
+            Err(e) => {
+                return Err(anyhow::Error::msg(format!(
+                    "Error getting playlist tracks: {}",
+                    e
+                )))
+            }
+        }
+    }
+}
+
 // returns true if the track is in the provided list of playlist items
 fn track_in_playlist(track: &FullTrack, playlist: &[PlaylistItem]) -> bool {
     playlist.iter().any(|playlist_item| {
@@ -60,7 +101,6 @@ fn track_in_playlist(track: &FullTrack, playlist: &[PlaylistItem]) -> bool {
         }
     })
 }
-
 
 // returns a list of tracks that are in the source playlist but not in the target playlist
 fn get_unsynced_tracks(
@@ -73,8 +113,7 @@ fn get_unsynced_tracks(
     // go over all the tracks in the source playlist and check if they are in the target playlist
     for source_item in source_playlist {
         if let Some(PlayableItem::Track(source_track)) = source_item.track {
-            let already_synced: bool =
-                track_in_playlist(&source_track, &target_playlist);
+            let already_synced: bool = track_in_playlist(&source_track, &target_playlist);
             if !already_synced {
                 unsynced_tracks.push(source_track);
             }
